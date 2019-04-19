58065bef57e28f92ab15c8e78fbea9969b8eb9ba
uknmr <kanamori@mediba.jp>
2019-04-20 04:43:01 +0900

    :sparkles: 相対時間を返すフィルターをつくる

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,32 @@
+const {
+  DateTime
+} = require('luxon')
+
+const translateDate = relativeDate => {
+  return relativeDate
+    .replace(/(?:in )?(\d+) second ago/, '$1 秒前')
+    .replace(/(?:in )?(\d+) seconds ago/, '$1 秒前')
+    .replace(/(?:in )?(\d+) minute ago/, '$1 分前')
+    .replace(/(?:in )?(\d+) minutes ago/, '$1 分前')
+    .replace(/(?:in )?(\d+) hour ago/, '$1 時間前')
+    .replace(/(?:in )?(\d+) hours ago/, '$1 時間前')
+    .replace(/(?:in )?(\d+) day ago/, '$1 日前')
+    .replace(/(?:in )?(\d+) days ago/, '$1 日前')
+    .replace(/(?:in )?(\d+) month ago/, '$1 月前')
+    .replace(/(?:in )?(\d+) months ago/, '$1 月前')
+    .replace(/(?:in )?(\d+) year ago/, '$1 年前')
+    .replace(/(?:in )?(\d+) years ago/, '$1 年前')
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('base', 'layouts/base.njk')
   eleventyConfig.addLayoutAlias('home', 'layouts/home.njk')
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
+
+  eleventyConfig.addFilter('readableDate', dateObj => {
+    const relativeDate = DateTime.fromJSDate(dateObj).toRelative()
+    return translateDate(relativeDate)
+  })
 
   return {}
 }
