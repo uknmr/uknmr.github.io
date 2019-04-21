02b80e25c983ca2051f5fea38ca72ea14dc40fb8
uknmr <kanamori@mediba.jp>
2019-04-21 15:40:22 +0900

    :construction_worker: CI でビルドする

---
+++ .circleci/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# gitの諸々の設定
+git config --global push.default simple
+git config --global user.email $(git --no-pager show -s --format='%ae' HEAD)
+git config --global user.name $CIRCLE_USERNAME
+
+if git status |grep 'docs/' >/dev/null; then
+  git add docs/
+  git commit --message ":rocket: GitHub Pages に自動でデプロイする #$CIRCLE_BUILD_NUM [skip ci]"
+  git push
+else
+  echo 'No change.'
+fi
