214b35ec0170e511109c4322f4bb08cf0dabff67
uknmr <kanamori@mediba.jp>
2019-04-23 10:52:20 +0900

    :bug: 静的ビルドしているので何日前みたいな書き方をやめる

--- .eleventy.js
+++ .eleventy.js
@@ -3,22 +3,6 @@ const {
 } = require('luxon')
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
 
-const translateDate = relativeDate => {
-  return relativeDate
-    .replace(/(?:in )?(\d+) second ago/, '$1 秒前')
-    .replace(/(?:in )?(\d+) seconds ago/, '$1 秒前')
-    .replace(/(?:in )?(\d+) minute ago/, '$1 分前')
-    .replace(/(?:in )?(\d+) minutes ago/, '$1 分前')
-    .replace(/(?:in )?(\d+) hour ago/, '$1 時間前')
-    .replace(/(?:in )?(\d+) hours ago/, '$1 時間前')
-    .replace(/(?:in )?(\d+) day ago/, '$1 日前')
-    .replace(/(?:in )?(\d+) days ago/, '$1 日前')
-    .replace(/(?:in )?(\d+) month ago/, '$1 月前')
-    .replace(/(?:in )?(\d+) months ago/, '$1 月前')
-    .replace(/(?:in )?(\d+) year ago/, '$1 年前')
-    .replace(/(?:in )?(\d+) years ago/, '$1 年前')
-}
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight)
 
@@ -27,8 +11,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addLayoutAlias('blog', 'layouts/blog.njk')
 
   eleventyConfig.addFilter('readableDate', dateObj => {
-    const relativeDate = DateTime.fromJSDate(dateObj).toRelative()
-    return translateDate(relativeDate)
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL)
   })
 
   eleventyConfig.addFilter('htmlDateString', dateObj => {
